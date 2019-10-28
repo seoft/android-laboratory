5f63a0f0bcbed7ad72cbd330426e72e65fd961a2
seoft <seoft.dev@gmail.com>
2019-10-28 21:50:45 +0900

    19.10.27 bak

---
+++ left-side-snappy-recyclerview/app/src/main/java/kr/co/seoft/left_side_snappy_recyclerview/MySnapHelper.kt
@@ -0,0 +1,30 @@
+//package kr.co.seoft.left_side_snappy_recyclerview
+//
+//import android.view.View
+//import androidx.recyclerview.widget.RecyclerView
+//import androidx.recyclerview.widget.SnapHelper
+//
+//class MySnapHelper : SnapHelper() {
+//    override fun calculateDistanceToFinalSnap(
+//        layoutManager: RecyclerView.LayoutManager,
+//        targetView: View
+//    ): IntArray? {
+//
+//
+//    }
+//
+//    override fun findTargetSnapPosition(
+//        layoutManager: RecyclerView.LayoutManager?,
+//        velocityX: Int,
+//        velocityY: Int
+//    ): Int {
+//
+//    }
+//
+//    override fun findSnapView(layoutManager: RecyclerView.LayoutManager?): View? {
+//
+//   }
+//
+//    fun Fi
+//
+//}
