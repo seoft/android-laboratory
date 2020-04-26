1883e1b8cd490d2491f103b68a25b6b6c7f47ea3
seoft <seoft.dev@gmail.com>
2020-04-26 14:06:33 +0900

    init write_post_with_items project

---
+++ writepostwithitems/app/src/main/java/kr/co/seoft/write_post_with_items/MainActivity.kt
@@ -0,0 +1,18 @@
+package kr.co.seoft.write_post_with_items
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+
+}
