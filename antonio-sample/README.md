bde3223598a55595a832ae242494846a5e3202b3
seoft <seoft.dev@gmail.com>
2021-11-13 20:11:13 +0900

    antonio_sample : 리드미 / art 추가 / AntonioUiModel 중복 개선

---
+++ antonio-sample/README.md
@@ -0,0 +1,4 @@
+# antonio-sample (리사이클러뷰 보일러 플레이트 최소화 방안)
+
+## 블로그
+https://seoft.tistory.com/88
--- antonio-sample/app/src/main/java/kr/co/seoft/antonio_sample/ui/antonio/AntonioUiModel.kt
+++ antonio-sample/app/src/main/java/kr/co/seoft/antonio_sample/ui/antonio/AntonioUiModel.kt
@@ -11,21 +11,20 @@ sealed class AntonioUiModel(
 ) : AntonioBindingModel {
     enum class Type { MONITOR, MOUSE, PHONE }
 
+    override fun bindingVariableId(): Int = BR.item
+
     data class Monitor(override val id: Long, override val price: Int, val inch: Float) :
         AntonioUiModel(Type.MONITOR, id, price) {
-        override fun bindingVariableId(): Int = BR.item
         override fun layoutId(): Int = R.layout.item_antonio_monitor
     }
 
     data class Mouse(override val id: Long, override val price: Int, val buttonCount: Int) :
         AntonioUiModel(Type.MOUSE, id, price) {
-        override fun bindingVariableId(): Int = BR.item
         override fun layoutId(): Int = R.layout.item_antonio_mouse
     }
 
     data class Phone(override val id: Long, override val price: Int, val os: String) :
         AntonioUiModel(Type.PHONE, id, price) {
-        override fun bindingVariableId(): Int = BR.item
         override fun layoutId(): Int = R.layout.item_antonio_phone
     }
 }
